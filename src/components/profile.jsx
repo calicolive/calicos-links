99652d45eb566b8bf557ee4abe9ff7e76957cc2e
Calicolive <calicodeep@gmail.com>
2022-12-08 08:31:00 -0500

    finished profile

---
+++ src/components/profile.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import {
+  FaTiktok,
+  FaYoutube,
+  FaInstagram,
+  FaSpotify,
+  FaEnvelope,
+} from 'react-icons/fa';
+
+export const Profile = () => {
+  return (
+    <section id='profile'>
+      {/* <!-- Profile Container --> */}
+      <div className='flex flex-col mx-auto items-center justify-center space-y-3 pt-20'>
+        {/* <!-- Profile Pic --> */}
+
+        <img
+          src='images/profile-pic.jpg'
+          alt='profile picture'
+          className='h-48 w-48 border-2 rounded-full border-indigo-400 object-cover'
+        />
+
+        {/* <!-- Tag --> */}
+        <h2 className='text-white text-2xl'>@calicolive</h2>
+        {/* <!-- Bio --> */}
+        <p className='text-sm text-indigo-200 w-48 text-center'>
+          Sound Designer | Developer
+        </p>
+
+        {/* <!-- Socials --> */}
+        <div className='flex space-x-4 mx-auto justify-evenly'>
+          {/* <!-- Youtube --> */}
+          <a href='https://youtube.com/@calicolive'>
+            <FaYoutube className='text-white h-6 w-6 hover:text-indigo-400 hover:scale-110' />
+          </a>
+          {/* <!-- Instagram --> */}
+          <a href='https://instagram.com/calicolive'>
+            <FaInstagram className='text-white h-6 w-6 hover:text-indigo-400 hover:scale-110' />
+          </a>
+          {/* <!-- Tik Tok --> */}
+          <a href='https://tiktok.com/@calicolive'>
+            <FaTiktok className='text-white h-6 w-6 hover:text-indigo-400 hover:scale-110' />
+          </a>
+          {/* <!-- Spotify --> */}
+          <a href='mailto:calicodeep@gmail.com'>
+            <FaEnvelope className='text-white h-6 w-6 hover:text-indigo-400 hover:scale-110' />
+          </a>
+        </div>
+      </div>
+    </section>
+  );
+};
